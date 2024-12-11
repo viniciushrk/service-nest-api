06f7476cecec6e1b57488fe258680a0dc19fbfb1
viniciushrk <viniciushrk@gmail.com>
2024-12-10 21:54:29 -0400

    fix: remove code unsed

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,10 @@
-import { Controller, Get, Res, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Response } from 'express';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(@Res() res: Response) {
